085e98f27c7b2ae150972963727b59723bbec631
bard.kim <bard.kim@kakaocorp.com>
2023-01-10 17:11:43 +0900

    feat: GetOrderedNotesUseCase 구현

--- app/src/main/java/com/survivalcoding/noteapp/domain/use_case/GetOrderedNotesUseCase.kt
+++ app/src/main/java/com/survivalcoding/noteapp/domain/use_case/GetOrderedNotesUseCase.kt
@@ -3,24 +3,24 @@ package com.survivalcoding.noteapp.domain.use_case
 import com.survivalcoding.noteapp.domain.model.Note
 import com.survivalcoding.noteapp.domain.repository.NoteRepository
 import com.survivalcoding.noteapp.domain.util.OrderType
+import com.survivalcoding.noteapp.domain.util.OrderType.TITLE
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class GetOrderedNotesUseCase(
     private val noteRepository: NoteRepository
 ) {
-//    operator fun invoke(orderType: OrderType, isAscending: Boolean): Flow<List<Note>> {
-//        return try {
-//            noteRepository.getNotes().map {
-//                when (orderType){
-//                     ->
-//                }
-//
-//
-//            }
-//
-//        } catch (e: Exception) {
-//
-//        }
-//    }
+    operator fun invoke(orderType: OrderType, isAscending: Boolean): Flow<List<Note>> {
+        return try {
+            noteRepository.getNotes().map { notes ->
+                when (orderType) {
+                    TITLE -> if (isAscending) notes.sortedBy { it.title } else notes.sortedByDescending { it.title }
+                    OrderType.DATE -> if (isAscending) notes.sortedBy { it.date } else notes.sortedByDescending { it.date }
+                    OrderType.COLOR -> if (isAscending) notes.sortedBy { it.color } else notes.sortedByDescending { it.color }
+                }
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
 }
