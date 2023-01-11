2146fd9fc52283f0eb1e618e13f46f3440d03fa7
comseung18 <40685565+comseung18@users.noreply.github.com>
2023-01-11 09:28:28 +0900

    test: NoteRepositoryImpl test 코드 작성

---
+++ app/src/androidTest/java/com/survivalcoding/noteapp/data/repository/NoteRepositoryImplTest.kt
@@ -0,0 +1,153 @@
+package com.survivalcoding.noteapp.data.repository
+
+import android.content.Context
+import android.graphics.Color
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import app.cash.turbine.test
+import com.survivalcoding.noteapp.data.data_source.NoteDao
+import com.survivalcoding.noteapp.data.data_source.NoteDatabase
+import com.survivalcoding.noteapp.domain.model.Note
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.*
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+import org.junit.runner.RunWith
+
+@Suppress("OPT_IN_USAGE")
+@RunWith(AndroidJUnit4::class)
+class NoteRepositoryImplTest {
+
+    private lateinit var noteDao: NoteDao
+    private lateinit var noteDatabase: NoteDatabase
+    private lateinit var noteRepository: NoteRepositoryImpl
+
+    private val noteDummy1 = Note("title1", "body1", Color.RED, System.currentTimeMillis(), id = 1)
+    private val noteDummy2 =
+        Note("title2", "body2", Color.GREEN, System.currentTimeMillis(), id = 2)
+    private val noteDummy3 = Note("title3", "body3", Color.BLUE, System.currentTimeMillis(), id = 3)
+
+    @get: Rule
+    val dispatcherRule = TestDispatcherRule()
+
+    @Before
+    fun setUp() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        noteDatabase = Room.inMemoryDatabaseBuilder(
+            context, NoteDatabase::class.java
+        ).build()
+        noteDao = noteDatabase.noteDao
+        noteRepository = NoteRepositoryImpl(noteDao)
+
+    }
+
+    @After
+    fun tearDown() {
+        noteDatabase.close()
+    }
+
+    @Test
+    fun getNote() = runTest {
+        noteRepository.addNote(noteDummy1)
+        noteRepository.addNote(noteDummy2)
+        noteRepository.addNote(noteDummy3)
+
+        val note1: Note = noteRepository.getNote(1)
+        assertEquals(noteDummy1, note1)
+
+        val note2: Note = noteRepository.getNote(2)
+        assertEquals(noteDummy2, note2)
+
+        val note3: Note = noteRepository.getNote(3)
+        assertEquals(noteDummy3, note3)
+    }
+
+    @Test
+    fun addNote() = runTest {
+        noteRepository.getNotes().test {
+            val notes = awaitItem()
+            assertEquals(0, notes.size)
+        }
+
+        noteRepository.addNote(noteDummy1)
+        noteRepository.addNote(noteDummy2)
+        noteRepository.getNotes().test {
+            val notes = awaitItem()
+            assertEquals(2, notes.size)
+            assertEquals(true, notes.contains(noteDummy1))
+            assertEquals(true, notes.contains(noteDummy2))
+        }
+    }
+
+    @Test
+    fun deleteNote() = runTest {
+        noteRepository.getNotes().test {
+            val notes = awaitItem()
+            assertEquals(0, notes.size)
+        }
+        noteRepository.addNote(noteDummy1)
+        noteRepository.addNote(noteDummy2)
+        noteRepository.getNotes().test {
+            val notes = awaitItem()
+            assertEquals(2, notes.size)
+        }
+        noteRepository.deleteNote(1)
+        noteRepository.getNotes().test {
+            val notes = awaitItem()
+            assertEquals(1, notes.size)
+            assertEquals(false, notes.contains(noteDummy1))
+            assertEquals(true, notes.contains(noteDummy2))
+        }
+        noteRepository.deleteNote(2)
+        noteRepository.getNotes().test {
+            val notes = awaitItem()
+            assertEquals(0, notes.size)
+            assertEquals(false, notes.contains(noteDummy1))
+            assertEquals(false, notes.contains(noteDummy2))
+        }
+    }
+
+    @Test
+    fun getNotes(): Unit = runTest {
+        noteRepository.addNote(noteDummy1)
+        noteRepository.addNote(noteDummy2)
+
+        noteRepository.getNotes().test {
+            val notes = awaitItem()
+            assertEquals(2, notes.size)
+            assertEquals(true, notes.contains(noteDummy1))
+            assertEquals(true, notes.contains(noteDummy2))
+            assertEquals(false, notes.contains(noteDummy3))
+        }
+
+        noteRepository.addNote(noteDummy3)
+        noteRepository.getNotes().test {
+            val notes = awaitItem()
+            assertEquals(3, notes.size)
+            assertEquals(true, notes.contains(noteDummy1))
+            assertEquals(true, notes.contains(noteDummy2))
+            assertEquals(true, notes.contains(noteDummy3))
+        }
+    }
+}
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class TestDispatcherRule(
+    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+) : TestWatcher() {
+    override fun starting(description: Description) {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description) {
+        Dispatchers.resetMain()
+    }
+}
